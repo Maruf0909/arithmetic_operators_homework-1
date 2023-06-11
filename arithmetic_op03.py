e4f9ae67deac5494a86be0689ab4e4aa28ca1b7d
Mac <mac@MacBook-Air-Mac.local>
2023-06-11 21:21:15 +0500

    are home3

--- arithmetic_op03.py
+++ arithmetic_op03.py
@@ -1,7 +1,15 @@
 #Create a variable called 'number' and assign it the two-digit number
+number = 72
 
 #Find the reverse of the number and assign it to a variable called 'answer'.
+n1 = number % 10
+n2 = number // 10
+
+answer = n1*10+n2
+
+
 
 #Print the value of the 'answer'
+print(answer)
 
 
